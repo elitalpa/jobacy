18968460eedf3a60db52bc63f3a2df7b41baee9d
elitalpa <elitalpa@gmail.com>
2023-12-21 11:38:26 +0100

    styling login page

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,10 +2,11 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
 const { isAuth } = require("../middleware/authMiddleware");
+const { requireAuth, checkJob, checkUser } = require('../middleware/authMiddleware');
 
-router.get('/signup', isAuth, authController.signup_get);
+router.get('/signup', isAuth, checkUser, authController.signup_get);
 router.post('/signup', isAuth, authController.signup_post);
-router.get('/login', isAuth, authController.login_get);
+router.get('/login', isAuth, checkUser, authController.login_get);
 router.post('/login', isAuth, authController.login_post);
 router.get('/logout', authController.logout_get);
 
